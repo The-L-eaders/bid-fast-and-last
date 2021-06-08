de2d82fcfd7ae15eb4a013ba71b08ea4340d7722
NihadZeidan <nihadzeidan@gmail.com>
2021-06-08 14:44:56 +0300

    test not done

--- __tests__/addToDB.test.js
+++ __tests__/addToDB.test.js
@@ -1,3 +1,18 @@
 'use strict';
-
+require('@code-fellows/supergoose');
 const supertest = require('supertest');
+const app = require('../brain.js');
+const fakeServer = supertest(app);
+
+
+describe('Testing /add route', () => {
+
+    it('Testing Body Data', async() => {
+        let data = { name: 'product1', price: 200, description: "testing product", image: "any", category: "car", timer: "120" }
+        let test = fakeServer.post('/add').send(data);
+
+        console.log(test.req)
+    });
+
+
+});
