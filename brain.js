ae96e4cb3bd265841a3c0722069b18d7372bc776
mohdAzzam <alazzamhmode@gmail.com>
2021-07-08 12:50:38 +0300

    sold function

--- brain.js
+++ brain.js
@@ -224,7 +224,40 @@ car.on("connection", (socket) => {
     });
     product = getProd[0];
   }
+  /**
+   * 
+   * @param {Object} data 
+   */
+   let sold = async (data)=>{
+    let getProduct = await productSchema.find({ _id: data.product._id });
 
+    const soldTo = {
+      name: getProduct[0].productName,
+      price: carLastPrice,
+      image: getProduct[0].productImage,
+      description: getProduct[0].productDis,
+    };
+
+    const dbUser = await userSchema.authenticateWithToken(lastToken);
+    const user = await userSchema.findByIdAndUpdate(
+      { _id: dbUser._id },
+      { $push: { cart: soldTo } }
+    );
+    let update = await userSchema.updateOne(
+      {
+        _id: getProduct[0].userId,
+        product: { $elemMatch: { _id: getProduct[0]._id } },
+      },
+      { $set: { "product.$.status": `sold  price ${carLastPrice} ` } }
+    );
+    let deleted = await productSchema.findByIdAndDelete({
+      _id: data.product._id,
+    });
+    lastToken = "";
+    carLastPrice = 0;
+
+    home.emit("soldEvent", soldTo);
+  }
   let notSold = async () => {
     let getProduct = await productSchema.find({ _id: product._id });
     let update = await userSchema.updateOne(
@@ -246,7 +279,8 @@ car.on("connection", (socket) => {
     let interval = setInterval(() => {
       if (obj.counter == 0) {
         if (lastToken != "") {
-          car.emit("try", { product, lastToken });
+          // car.emit("try", { product, lastToken });
+          sold({product, lastToken});
         } else {
           notSold();
         }
